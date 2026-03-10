a899785bfb2ce921685a7ad3645a0fd0d34f3c41
Rafi <89698848+salirafi@users.noreply.github.com>
2026-03-10 11:51:58 +0900

    Update fetch_bmkg_data.bat

--- fetch_bmkg_data.bat
+++ fetch_bmkg_data.bat
@@ -1,3 +1,2 @@
 @echo off
-cd /d C:\Users\salir\OneDrive\Documents\heat_risk_personal_project
-C:\Users\salir\AppData\Local\Microsoft\WindowsApp\python.exe fetch_bmkg_data_jakarta.py
+python .\src\fetch_bmkg_data_jakarta.py
