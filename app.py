2b0ca2c1c478999d91271de69a5b83d3e46c2ebb
Rafi <89698848+salirafi@users.noreply.github.com>
2026-03-10 14:32:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ Create the web app.
 '''
   
 from shiny import App
-from ui.layout import app_ui
-from ui.app_server import server
+from app.ui.layout import app_ui
+from app.ui.app_server import server
 
 app = App(app_ui, server)
 
