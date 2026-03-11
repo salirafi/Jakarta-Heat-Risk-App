d5a597b0bcc9ab43a9926e87ed5e857bc33a9ea3
Rafi <89698848+salirafi@users.noreply.github.com>
2026-03-11 21:26:59 +0900

    Delete src_app directory

--- src_app/components.py
+++
@@ -1,8 +0,0 @@
-def guide_button_id(level: str) -> str:
-    return {
-        "Lower Risk": "guide_lower_risk",
-        "Caution": "guide_caution",
-        "Extreme Caution": "guide_extreme_caution",
-        "Danger": "guide_danger",
-        "Extreme Danger": "guide_extreme_danger",
-    }[level]
